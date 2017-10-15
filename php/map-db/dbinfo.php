0706c36caccd8c6365a9ddde345e1e89e12b3119
smeesathit <smeesathit.gmail.com>
2017-10-15 19:31:34 +0700

    PHP lab RSS and Map with database
    Signed-off-by: smeesathit <smeesathit.gmail.com>

---
+++ php/map-db/dbinfo.php
@@ -0,0 +1,5 @@
+<?php
+$username="username"; // replace with your database connection info 
+$password="password";
+$database="database";
+?>
---
+++ php/map-db/php-dom.php
@@ -0,0 +1,55 @@
+<?php
+
+require("dbinfo.php");
+
+// Start XML file, create parent node
+
+$dom = new DOMDocument("1.0");
+$node = $dom->createElement("markers");
+$parnode = $dom->appendChild($node);
+
+// Opens a connection to a MySQL server
+
+$connection=mysql_connect ('your database URL', $username, $password);
+if (!$connection) {  die('Not connected : ' . mysql_error());}
+
+// Set the active MySQL database
+
+$db_selected = mysql_select_db($database, $connection);
+if (!$db_selected) {
+  die ('Can\'t use db : ' . mysql_error());
+}
+// set thai encoding
+mysql_query("SET name UTF8");
+mysql_query("SET address UTF8");
+mysql_query("SET character_set_results=utf8");
+mysql_query("SET character_set_client=utf8");
+mysql_query("SET character_set_connection=utf8");
+
+// Select all the rows in the markers table
+
+$query = "SELECT * FROM markersakon WHERE 1";
+$result = mysql_query($query);
+if (!$result) {
+  die('Invalid query: ' . mysql_error());
+}
+
+header("Content-type: text/xml");
+
+// Iterate through the rows, adding XML nodes for each
+
+while ($row = @mysql_fetch_assoc($result)){
+  // Add to XML document node
+  $node = $dom->createElement("marker");
+  $newnode = $parnode->appendChild($node);
+  $newnode->setAttribute("id",$row['id']);
+  $newnode->setAttribute("name",$row['name']);
+  $newnode->setAttribute("address", $row['address']);
+  $newnode->setAttribute("lat", $row['lat']);
+  $newnode->setAttribute("lng", $row['lng']);
+  $newnode->setAttribute("type", $row['type']);
+}
+
+echo $dom->saveXML();
+
+?>
